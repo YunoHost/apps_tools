06333171265d984d414d1601f2dec81ea891f969
oleole39 <59071673+oleole39@users.noreply.github.com>
2025-04-23 04:02:35 +0200

    readme_generator: check that logo exists

--- readme_generator/make_readme.py
+++ readme_generator/make_readme.py
@@ -7,12 +7,24 @@ from pathlib import Path
 import toml
 from jinja2 import Environment, FileSystemLoader
 
+import requests
+
 # add apps/tools to sys.path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 README_GEN_DIR = Path(__file__).resolve().parent
 
-
+def url_exists(url):
+    try:
+        get = requests.get(url)
+        if get.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        raise Exception("Can't check URL'")
+        
+        
 def generate_READMEs(app_path: Path):
     if not app_path.exists():
         raise Exception("App path provided doesn't exists ?!")
@@ -24,7 +36,7 @@ def generate_READMEs(app_path: Path):
     )
     template = env.get_template("README.md.j2")
 
-    out: str = template.render(manifest=manifest)
+    out: str = template.render(manifest=manifest, url_exists=url_exists)
     (app_path / "README.md").write_text(out)
 
     # Delete legacy READMEs
