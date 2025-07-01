ea8df69360bb69d8e1d81c4508c3d7f4ff0a71ca
Alexandre Aubin <alex.aubin@mailoo.org>
2025-07-02 00:26:24 +0200

    Nierf

--- list_builder.py
+++ list_builder.py
@@ -211,7 +211,7 @@ def build_app_dict(app, infos, cache_path: Path):
         if branch != "testing":
             continue
         try:
-            ahead = repo.is_ancestor(result_infos["commit"], infos["branch"])
+            ahead = not repo.is_ancestor(result_infos["commit"], infos["branch"])
         except Exception:
             # This will typically fail if the ref of the commit is unknown because it's only a single-branch checkout
             # ... BUT it could also be a super-old commit and we only have the last X commits in our checkout to optimize space hmpf
