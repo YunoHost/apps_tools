4c1f0b5e10921c3b23cefd53fb6fdc3488c65d0e
tituspijean <tituspijean@yunohost.org>
2025-07-26 11:52:35 +0200

    Fix changelog helper bot
    mix up between GitPython and PyGithub

--- webhooks/webhook.py
+++ webhooks/webhook.py
@@ -252,13 +252,15 @@ def add_changelog(request: Request, pr_infos: dict, changelog=None) -> HTTPRespo
         manifest_file = folder / "manifest.toml"
         manifest = tomlkit.load(manifest_file.open("r", encoding="utf-8"))
         version = manifest["version"]
+        file = f"{folder}/doc/PRE_UPGRADE.d/{version}.md"
 
-        repo.create_file(
-            f"doc/PRE_UPGRADE.d/{version}.md",
+        with open(file, "a") as f:
+            f.write(f"{changelog}")
+
+        repo.git.add(file)
+        repo.index.commit(
             f"Add pre_upgrade message for {version}",
-            f"{changelog}",
             author=Actor("yunohost-bot", "yunohost@yunohost.org"),
-            branch=branch,
         )
 
         logging.debug(f"Pushing {repository}")
