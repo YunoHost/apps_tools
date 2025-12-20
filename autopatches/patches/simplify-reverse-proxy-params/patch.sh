e737215939a4be957a45a967b1112e72a32aa5b3
Alexandre Aubin <alex.aubin@mailoo.org>
2025-12-20 20:51:45 +0100

    Add an autopatch to simplify proxy_pass / fascgi_pass options in nginx.conf

---
+++ autopatches/patches/simplify-reverse-proxy-params/patch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+python3 $SCRIPT_DIR/patch.py ./
+
+git add .
---
+++ autopatches/patches/simplify-reverse-proxy-params/pr_title.md
@@ -0,0 +1,1 @@
+Automatic patch to simplify `proxy_pass`/`fastcgi_pass` options in nginx.conf
